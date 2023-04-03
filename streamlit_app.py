0444b476c95af60f8bb2cc2208115122580b3ecb
Previn27 <129780621+Previn27@users.noreply.github.com>
2023-04-03 19:18:26 +0200

    Update streamlit_app.py
    Build smoothie added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 st.title("My Parents New Healthy Diner")
+
 st.header('Breakfast Favorites')
+
 st.text("🥣 Omega 3 & Blueberry Oatmeal")
 st.text("🥗 Kale, Spinach and Rocket Smoothie")
 st.text("🐔 Hard-Boiled Free-Range Egg")
 st.text("🥑🍞 Avocado Toast")
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

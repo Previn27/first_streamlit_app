5d8e6bda470592a56cc8f3792f83db67dff9e514
Previn27 <129780621+Previn27@users.noreply.github.com>
2023-04-03 19:11:38 +0200

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("My Parents New Healthy Diner")
 st.subheader('Breakfast Menu')
+st.write("Omega 3 & Blueberry Oatmeal")
+st.write("Kale, Spinach and Rocket Smoothie")
+st.write("Hard-Boiled Free-Range Egg")
